a4049bc2cf3fe290611e585deafdfdc46cb5bc6b
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2022-08-04 20:28:45 +0530

    Added new interfaces: IUnitOfWork and IRepositoryAsync

---
+++ Fluid.Core/Interfaces/Repositories/IRepositoryAsync.cs
@@ -0,0 +1,24 @@
+﻿using Fluid.Shared.Contracts;
+
+namespace Fluid.Core.Interfaces.Repositories;
+
+public interface IRepositoryAsync<T> where T : class, IEntity
+{
+    IQueryable<T> Entities { get; }
+
+    Task<T> AddAsync(T entity);
+
+    Task<int> CountAsync();
+
+    Task<int> CountAsync(Expression<Func<T, bool>> predicate);
+
+    Task DeleteAsync(T entity);
+
+    Task<List<T>> GetAllAsync();
+
+    Task<T> GetByIdAsync<TId>(TId id);
+
+    Task<List<T>> GetPagedResponseAsync(int pageNumber, int pageSize);
+
+    Task UpdateAsync<TId>(T updatedEntity, TId id);
+}
---
+++ Fluid.Core/Interfaces/Repositories/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using Fluid.Shared.Contracts;
+
+namespace Fluid.Core.Interfaces.Repositories;
+
+public interface IUnitOfWork : IDisposable
+{
+    IRepositoryAsync<T> GetRepository<T>() where T : class, IEntity;
+
+    Task<int> Commit();
+}
