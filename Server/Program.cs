a41024a440f4d48b5959a316c9a5e1dad1b4c708
Sathiyaraman <Sathiyaraman2003@gmail.com>
2022-08-04 12:35:46 +0530

    Removed unused files in Server project

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,10 +1,8 @@
-using Microsoft.AspNetCore.ResponseCompression;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllers();
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
