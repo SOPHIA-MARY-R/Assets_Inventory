84e5873acfab7d096b5107d268f9d3a9809aaf20
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2022-08-05 17:10:20 +0530

    Added Specification Pattern for Filtering: ISpecification, BaseSpecification and Composite Specifications

---
+++ Fluid.Core/Specifications/Base/ISpecification.cs
@@ -0,0 +1,12 @@
+﻿namespace Fluid.Core.Specifications.Base;
+
+public interface ISpecification<T> where T : class, IEntity
+{
+    Expression<Func<T, bool>> FilterCondition { get; }
+
+    List<Expression<Func<T, object>>> Includes { get; }
+
+    List<string> IncludeStrings { get; }
+
+    bool IsSatisfiedBy(T entity);
+}
