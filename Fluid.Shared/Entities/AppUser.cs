515880abdc88d508ccd9a871bc7ce566a033e7f1
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2022-08-06 23:16:51 +0530

    Added AppUser entity to replace default IdentityUser with additional fields for refresh token

---
+++ Fluid.Shared/Entities/AppUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Fluid.Shared.Entities;
+
+public class AppUser : IdentityUser
+{
+    public string RefreshToken { get; set; }
+    public DateTime RefreshTokenExpiryTime { get; set; } = DateTime.MinValue;
+}
