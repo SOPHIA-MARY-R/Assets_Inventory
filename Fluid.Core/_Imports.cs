75ddb0ec9e26a699de9ceeecb2b81a46282582f7
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2022-08-06 14:36:51 +0530

    Added new global usings in _Imports.cs in Fluid.Core

--- Fluid.Core/_Imports.cs
+++ Fluid.Core/_Imports.cs
@@ -1,3 +1,6 @@
 ﻿global using Fluid.Core.Interfaces.Repositories;
+global using Fluid.Shared.Contracts;
+global using Fluid.Shared.Wrapper;
 global using Microsoft.EntityFrameworkCore;
+global using System.Linq.Dynamic.Core;
 global using System.Linq.Expressions;
