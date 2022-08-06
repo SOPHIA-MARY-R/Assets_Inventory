8854a6ea3babaffd57568c3db2c9b78c15bd3071
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2022-08-06 23:15:35 +0530

    Added UserConstants that have predetermined AdminUser credentials

---
+++ Fluid.Shared/Constants/UserConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Fluid.Shared.Constants;
+
+public class UserConstants
+{
+    public const string AdminUserName = "admin";
+    public const string AdminEmail = "admin@niot2.res.in";
+    public const string Password = "Admin@123";
+}
