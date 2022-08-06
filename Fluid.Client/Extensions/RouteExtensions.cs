092aab3f4846fd50ec16588d44908f547c491180
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2022-08-06 14:18:36 +0530

    Added RouteExtensions to append querystring in url

---
+++ Fluid.Client/Extensions/RouteExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Fluid.Shared.Requests;
+
+namespace Fluid.Client.Extensions;
+
+public static class RouteExtensions
+{
+    public static string ToPagedRoute(this string url, PagedRequest request)
+    {
+        url += $"?pageNumber={request.PageNumber}&pageSize={request.PageSize}&searchString={request.SearchString}&orderBy=";
+        if (request.OrderBy?.Any() == true)
+        {
+            foreach (var orderByPart in request.OrderBy)
+            {
+                url += $"{orderByPart},";
+            }
+            url = url[..^1];
+        }
+        return url;
+    }
+}
