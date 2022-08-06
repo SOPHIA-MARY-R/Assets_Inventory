adf6b51e2c774efb1818ce73ee9309843f072b4e
SOPHIA MARY R <soph20410.cs@rmkec.ac.in>
2022-08-06 13:41:05 +0530

    Added icons in assets dialogs, added MudSelect for Enum type, Update actions column and updated _Imports.cs of Fluid.Core

--- Fluid.Core/_Imports.cs
+++ Fluid.Core/_Imports.cs
@@ -1,3 +1,5 @@
 ﻿global using Fluid.Core.Interfaces.Repositories;
 global using Microsoft.EntityFrameworkCore;
 global using System.Linq.Expressions;
+global using Fluid.Shared.Contracts;
+global using Fluid.Shared.Wrapper;
