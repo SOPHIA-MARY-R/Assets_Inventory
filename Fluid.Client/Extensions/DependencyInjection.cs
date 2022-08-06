2adef1f3a07ec18f71b9762845bdd89ae03c0b7c
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2022-08-06 14:34:29 +0530

    Added the KeyboardMasterHttpClient to the blazor app's DI container

---
+++ Fluid.Client/Extensions/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using Fluid.Client.Services.HttpClients;
+
+namespace Fluid.Client.Extensions;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddHttpClients(this IServiceCollection services, string baseAddress)
+    {
+        services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
+        services.AddTransient<KeyboardMasterHttpClient>();
+        return services;
+    }
+}
