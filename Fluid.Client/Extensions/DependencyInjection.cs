7ad53029e57b07d26c827736e12153d75e330bb1
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2022-08-07 09:27:59 +0530

    Added all services to the client app DI container and properly segregated all of them

--- Fluid.Client/Extensions/DependencyInjection.cs
+++ Fluid.Client/Extensions/DependencyInjection.cs
@@ -1,14 +1,43 @@
-﻿using Fluid.Client.Services.HttpClients;
+﻿using Blazored.LocalStorage;
+using Fluid.Client.Authentication;
+using Fluid.Client.Services;
+using Fluid.Client.Services.HttpClients;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
 
 namespace Fluid.Client.Extensions;
 
 public static class DependencyInjection
 {
+    public static WebAssemblyHostBuilder AddClientServices(this WebAssemblyHostBuilder builder)
+    {
+        builder.Services.AddAuthentication();
+        builder.Services.AddHttpClients(builder.HostEnvironment.BaseAddress);
+        builder.Services.AddMudServices();
+        builder.Services.AddBlazoredLocalStorage();
+        return builder;
+    }
+
+    public static IServiceCollection AddAuthentication(this IServiceCollection services)
+    {
+        services.AddAuthorizationCore();
+        services.AddTransient<AuthorizationHeaderHandler>();
+        services.AddScoped<AuthenticationStateProvider, TechnicianAuthStateProvider>();
+        services.AddScoped<TechnicianAuthStateProvider>();
+        return services;
+    }
+
     public static IServiceCollection AddHttpClients(this IServiceCollection services, string baseAddress)
     {
-        services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
+        services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("Fluid.API").EnableIntercept(sp));
+        services.AddHttpClient("Fluid.API", client => client.BaseAddress = new Uri(baseAddress)).AddHttpMessageHandler<AuthorizationHeaderHandler>();
+        services.AddHttpClientInterceptor();
         services.AddTransient<KeyboardMasterHttpClient>();
         services.AddTransient<MouseMasterHttpClient>();
+        services.AddTransient<UserHttpClient>();
+        services.AddTransient<HttpClientInterceptorManager>();
         return services;
     }
 }
