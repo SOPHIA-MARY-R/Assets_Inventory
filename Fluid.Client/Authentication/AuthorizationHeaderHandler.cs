df1943918ac5d6a796638f6d739ecaa7668fcf89
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2022-08-07 09:23:48 +0530

    Added AuthorizationHeaderHandler which is responsible for setting Authorization headers for HttpClient

---
+++ Fluid.Client/Authentication/AuthorizationHeaderHandler.cs
@@ -0,0 +1,28 @@
+﻿using Blazored.LocalStorage;
+using Fluid.Shared.Constants;
+using System.Net.Http.Headers;
+
+namespace Fluid.Client.Authentication;
+
+public class AuthorizationHeaderHandler : DelegatingHandler
+{
+    private readonly ILocalStorageService _localStorageService;
+
+    public AuthorizationHeaderHandler(ILocalStorageService localStorageService)
+    {
+        _localStorageService = localStorageService;
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        if (request.Headers.Authorization?.Scheme != "Bearer")
+        {
+            var savedToken = await _localStorageService.GetItemAsync<string>(StorageConstants.AuthToken);
+            if (!string.IsNullOrWhiteSpace(savedToken))
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", savedToken);
+            }
+        }
+        return await base.SendAsync(request, cancellationToken);
+    }
+}
