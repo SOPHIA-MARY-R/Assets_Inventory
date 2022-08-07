22b5e9de4fda22abd33d5ddce2e8a952337f09dd
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2022-08-07 09:36:09 +0530

    Added Login Page

---
+++ Fluid.Client/Pages/Login.razor.cs
@@ -0,0 +1,46 @@
+﻿using Fluid.Shared.Requests;
+using MudBlazor;
+
+namespace Fluid.Client.Pages;
+
+public partial class Login
+{
+    private LoginRequest _requestModel = new();
+
+    private async Task SubmitAsync()
+{
+        var result = await userHttpClient.Login(_requestModel);
+        if (result.Succeeded)
+        {
+            snackbar.Add(string.Format("Welcome {0}", _requestModel.UserName), Severity.Success);
+            navigationManager.NavigateTo("/", true);
+        }
+        else
+        {
+            foreach (var message in result.Messages)
+            {
+                snackbar.Add(message, Severity.Error);
+            }
+        }
+    }
+
+    private bool _passwordVisibility;
+    private InputType _passwordInput = InputType.Password;
+    private string _passwordInputIcon = Icons.Material.Filled.VisibilityOff;
+
+    void TogglePasswordVisibility()
+    {
+        if (_passwordVisibility)
+        {
+            _passwordVisibility = false;
+            _passwordInputIcon = Icons.Material.Filled.VisibilityOff;
+            _passwordInput = InputType.Password;
+        }
+        else
+        {
+            _passwordVisibility = true;
+            _passwordInputIcon = Icons.Material.Filled.Visibility;
+            _passwordInput = InputType.Text;
+        }
+    }
+}
