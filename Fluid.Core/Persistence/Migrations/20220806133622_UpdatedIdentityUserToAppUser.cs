9bd27dbe307fb51f773b7fde1ac697c904821737
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2022-08-07 08:56:28 +0530

    Added Database Migrations for updated AppUser

---
+++ Fluid.Core/Persistence/Migrations/20220806133622_UpdatedIdentityUserToAppUser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fluid.Core.Persistence.Migrations
+{
+    public partial class UpdatedIdentityUserToAppUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                schema: "dbo",
+                table: "Technicians",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RefreshTokenExpiryTime",
+                schema: "dbo",
+                table: "Technicians",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                schema: "dbo",
+                table: "Technicians");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshTokenExpiryTime",
+                schema: "dbo",
+                table: "Technicians");
+        }
+    }
+}
