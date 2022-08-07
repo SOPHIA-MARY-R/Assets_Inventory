fa6db66f60dba04966ba4ada47138b440f6da209
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2022-08-07 09:19:40 +0530

    Added TechnicianController for Technician user-related services

---
+++ Fluid.Server/Controllers/TechnicianController.cs
@@ -0,0 +1,40 @@
+﻿using Fluid.Core.Features;
+using Fluid.Shared.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fluid.Server.Controllers;
+
+[ApiController]
+public class TechnicianController : ControllerBase
+{
+    private readonly ITechnicianUserService _userService;
+
+    public TechnicianController(ITechnicianUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpPost("api/technician/login")]
+    public async Task<IActionResult> Login(LoginRequest loginRequest)
+    {
+        return Ok(await _userService.Login(loginRequest));
+    }
+
+    [HttpPost("api/technician/register")]
+    public async Task<IActionResult> Register(RegisterRequest registerRequest)
+    {
+        return Ok(await _userService.RegisterAsync(registerRequest));
+    }
+
+    [HttpPost("api/technician/forgot-password")]
+    public async Task<IActionResult> ForgotPassword(ForgotPasswordRequest forgotPasswordRequest)
+    {
+        return Ok(await _userService.ForgotPasswordAsync(forgotPasswordRequest, Request.Headers["origin"]));
+    }
+
+    [HttpPost("api/technician/refresh-token")]
+    public async Task<IActionResult> RefreshToken(RefreshTokenRequest refreshTokenRequest)
+    {
+        return Ok(await _userService.RefreshToken(refreshTokenRequest));
+    }
+}
