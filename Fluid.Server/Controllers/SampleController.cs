d2056d5cde4615fbea7d9b89faa6adaa5fb823de
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2022-08-07 08:52:01 +0530

    Added a plain "GreetMe" controller endpoint for separate purpose

---
+++ Fluid.Server/Controllers/SampleController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fluid.Server.Controllers;
+
+[ApiController]
+public class SampleController : ControllerBase
+{
+    [HttpGet("api/sample/greet")]
+    public IActionResult Greeting()
+    {
+        return Ok($"Pinging from client {Request.Host} is working successfully");
+    }
+}
