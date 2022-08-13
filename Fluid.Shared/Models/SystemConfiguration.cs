ee89938af5e3191f41fdad7e7769614ee2150101
Sathiyaraman <Sathiyaraman2003@gmail.com>
2022-08-13 14:33:42 +0530

    Added SetupDetails page and streamlined the login process

---
+++ Fluid.Shared/Models/SystemConfiguration.cs
@@ -0,0 +1,32 @@
+﻿namespace Fluid.Shared.Models;
+
+public class SystemConfiguration
+{
+    public SystemConfiguration()
+    {
+        MachineModel = new();
+        Motherboard = new();
+        PhysicalMemories = new();
+        HardDisks = new();
+        Processors = new();
+        Mouse = new();
+        Keyboard = new();
+        Monitor = new();
+    }
+
+    public MachineMasterModel MachineModel { get; set; }
+
+    public MotherboardModel Motherboard { get; set; }
+
+    public List<PhysicalMemoryModel> PhysicalMemories { get; set; }
+
+    public List<HardDiskModel> HardDisks { get; set; }
+
+    public List<ProcessorModel> Processors { get; set; }
+
+    public MouseModel Mouse { get; set; }
+
+    public KeyboardModel Keyboard { get; set; }
+
+    public MonitorModel Monitor { get; set; }
+}
