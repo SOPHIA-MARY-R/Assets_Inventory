ea9817745f53d8eb7714458feb43eab0e6a511ca
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2022-08-14 15:35:14 +0530

    Added new controller: FeedLogController

---
+++ Fluid.Server/Controllers/FeedLogController.cs
@@ -0,0 +1,23 @@
+﻿using Fluid.Core.Features;
+using Fluid.Shared.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fluid.Server.Controllers;
+
+[Route("api/feedlog/feed")]
+[ApiController]
+public class FeedLogController : ControllerBase
+{
+    private readonly IFeedLogService _feedLogService;
+
+    public FeedLogController(IFeedLogService feedLogService)
+    {
+        _feedLogService = feedLogService;
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> FeedSystemConfig(SystemConfiguration systemConfiguration)
+    {
+        return Ok(await _feedLogService.SaveLog(systemConfiguration));
+    }
+}
