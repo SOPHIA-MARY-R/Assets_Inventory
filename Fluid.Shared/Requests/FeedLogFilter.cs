980f18786f228485b8bad15c9a13e52e806d2d39
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2022-08-14 23:44:10 +0530

    Added FeedLogFilter.cs model class

---
+++ Fluid.Shared/Requests/FeedLogFilter.cs
@@ -0,0 +1,16 @@
+﻿namespace Fluid.Shared.Requests;
+
+public class FeedLogFilter
+{
+    public int PageNumber { get; set; }
+    public int PageSize { get; set; }
+    public string AssetTag { get; set; }
+    public string MachineName { get; set; }
+    public string AssignedPersonName { get; set; }
+    public string AssetLocation { get; set; }
+    public string AssetBranch { get; set; }
+    public long FromDateTimeTicks { get; set; }
+    public long ToDateTimeTicks { get; set; }
+    public LogAttendStatus? LogAttendStatus { get; set; }
+    public MachineType? MachineType { get; set; }
+}
