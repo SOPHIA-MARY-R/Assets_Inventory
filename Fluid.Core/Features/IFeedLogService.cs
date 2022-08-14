b2dc0474b1a38730c53dcef881dbdedf1490284b
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2022-08-14 23:45:20 +0530

    Updated FeedLogService.cs with GetAllAsync and AttendLog method

--- Fluid.Core/Features/IFeedLogService.cs
+++ Fluid.Core/Features/IFeedLogService.cs
@@ -1,8 +1,14 @@
-﻿using Fluid.Shared.Models;
+﻿using Fluid.Shared.Entities;
+using Fluid.Shared.Models;
+using Fluid.Shared.Requests;
 
 namespace Fluid.Core.Features;
 
 public interface IFeedLogService
 {
     Task<IResult> SaveLog(SystemConfiguration systemConfiguration);
+
+    Task<PaginatedResult<FeedLog>> GetAllAsync(int pageNumber, int pageSize, FeedLogFilter filter);
+
+    Task<IResult> AttendLog(FeedLog feedLog);
 }
