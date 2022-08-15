8977e0acb4b5c3f7b647acf74a1085a4fce09c7d
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2022-08-15 14:51:24 +0530

    Added BackgroundLoggerService.cs and streamlined the SystemConfigurationService.cs to coordinate with sending logs

---
+++ Fluid.BgService/Services/BackgroundLoggerService.cs
@@ -0,0 +1,24 @@
+﻿using Fluid.BgService.Models;
+
+namespace Fluid.BgService.Services;
+
+public class BackgroundLoggerService : BackgroundService
+{
+    private readonly WritableOptions<BackgroundLogTime> _options;
+    private readonly SystemConfigurationService _systemConfigurationService;
+
+    public BackgroundLoggerService(WritableOptions<BackgroundLogTime> options, SystemConfigurationService systemConfigurationService)
+    {
+        _options = options;
+        _systemConfigurationService = systemConfigurationService;
+    }
+    
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            if (DateTime.Now < _options.Value.NextLogDateTime || _systemConfigurationService.IsWriting) continue;
+            await _systemConfigurationService.LogSystemConfiguration();
+        }
+    }
+}
