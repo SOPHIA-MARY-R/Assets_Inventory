64e70a25cf09fb53b239cb374cacf4cb69259adf
SuhailSherief <suhailscreed@gmail.com>
2022-08-18 10:51:22 +0530

    Added new component: AddEditMachineInfo.razor

---
+++ Fluid.Client/Pages/AddEditMachineInfo.razor.cs
@@ -0,0 +1,18 @@
+﻿using Fluid.Shared.Models;
+
+namespace Fluid.Client.Pages;
+
+public partial class AddEditMachineInfo
+{
+    public string Id { get; set; }
+    private SystemConfiguration _systemConfiguration = new SystemConfiguration();
+    protected override Task OnInitializedAsync()
+    {
+        if (!string.IsNullOrEmpty(Id))
+        {
+            
+        }
+        return base.OnInitializedAsync();
+    }
+    
+}
