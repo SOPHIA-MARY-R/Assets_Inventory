6bbff5f4ce6186774038a4f6fad4486dfa27dc9b
Sathiyaraman M <sathiyaraman2003@gmail.com>
2022-08-18 10:08:25 +0530

    Updated UnitOfWork.cs to include new AppDbContext property

--- Fluid.Core/Interfaces/Repositories/IUnitOfWork.cs
+++ Fluid.Core/Interfaces/Repositories/IUnitOfWork.cs
@@ -1,10 +1,11 @@
-﻿using Fluid.Shared.Contracts;
+﻿using Fluid.Core.Persistence;
+using Fluid.Shared.Contracts;
 
 namespace Fluid.Core.Interfaces.Repositories;
 
 public interface IUnitOfWork : IDisposable
 {
+    AppDbContext AppDbContext { get; }
     IRepositoryAsync<T> GetRepository<T>() where T : class, IEntity;
-
     Task<int> Commit();
 }
