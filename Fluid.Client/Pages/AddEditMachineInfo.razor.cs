4b983177b71797fb2ed57e894c98413f0057dce2
SuhailSherief <suhailscreed@gmail.com>
2022-08-19 17:45:00 +0530

    Updated AddEditMachineInfo.razor

--- Fluid.Client/Pages/AddEditMachineInfo.razor.cs
+++ Fluid.Client/Pages/AddEditMachineInfo.razor.cs
@@ -1,18 +1,35 @@
-﻿using Fluid.Shared.Models;
+﻿using Fluid.Shared.Enums;
+using Fluid.Shared.Models;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
 
 namespace Fluid.Client.Pages;
 
 public partial class AddEditMachineInfo
 {
+    private HardwareSelectionType motherboardSelectionType = HardwareSelectionType.Existing;
+    [Parameter]
     public string Id { get; set; }
+    private SystemConfiguration Model = new SystemConfiguration(); 
     private SystemConfiguration _systemConfiguration = new SystemConfiguration();
-    protected override Task OnInitializedAsync()
+    protected async override Task OnInitializedAsync()
     {
         if (!string.IsNullOrEmpty(Id))
         {
-            
+            var result = await MasterHttpClient.GetByIdAsync(Id);
+            if (result.Succeeded)
+            {
+                Model = result.Data;
+            }
+            else
+            {
+                foreach (var message in result.Messages)
+                {
+                    snackbar.Add(message, Severity.Error);
+                }
+            }
         }
-        return base.OnInitializedAsync();
+        await base.OnInitializedAsync();
     }
     
 }
