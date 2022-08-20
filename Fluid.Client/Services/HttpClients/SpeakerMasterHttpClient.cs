aa3b38eb8a33ce0bdfa5d6f23983296f5527c677
SuhailSherief <suhailscreed@gmail.com>
2022-08-20 12:05:03 +0530

    Removed redundant SpeakerModel.cs and replaced all its usages with SpeakerInfo class

--- Fluid.Client/Services/HttpClients/SpeakerMasterHttpClient.cs
+++ Fluid.Client/Services/HttpClients/SpeakerMasterHttpClient.cs
@@ -3,6 +3,7 @@ using Fluid.Shared.Models;
 using Fluid.Shared.Requests;
 using Fluid.Shared.Wrapper;
 using System.Net.Http.Json;
+using Fluid.Shared.Entities;
 
 namespace Fluid.Client.Services.HttpClients;
 
@@ -12,25 +13,25 @@ public class SpeakerMasterHttpClient
 
     public SpeakerMasterHttpClient(HttpClient httpClient) => _httpClient = httpClient;
 
-    public async Task<PaginatedResult<SpeakerModel>> GetAllAsync(PagedRequest pagedRequest)
+    public async Task<PaginatedResult<SpeakerInfo>> GetAllAsync(PagedRequest pagedRequest)
     {
         var response = await _httpClient.GetAsync("api/masters/speakers".ToPagedRoute(pagedRequest));
-        return await response.ToPaginatedResult<SpeakerModel>();
+        return await response.ToPaginatedResult<SpeakerInfo>();
     }
 
-    public async Task<IResult<SpeakerModel>> GetByIdAsync(string oemSerialNo)
+    public async Task<IResult<SpeakerInfo>> GetByIdAsync(string oemSerialNo)
     {
         var response = await _httpClient.GetAsync($"api/masters/speakers/{oemSerialNo}");
-        return await response.ToResult<SpeakerModel>();
+        return await response.ToResult<SpeakerInfo>();
     }
 
-    public async Task<IResult<string>> AddAsync(SpeakerModel model)
+    public async Task<IResult<string>> AddAsync(SpeakerInfo model)
     {
         var response = await _httpClient.PostAsJsonAsync("api/masters/speakers", model);
         return await response.ToResult<string>();
     }
 
-    public async Task<IResult<string>> EditAsync(SpeakerModel model)
+    public async Task<IResult<string>> EditAsync(SpeakerInfo model)
     {
         var response = await _httpClient.PutAsJsonAsync($"api/masters/speakers/{model.OemSerialNo}", model);
         return await response.ToResult<string>();
