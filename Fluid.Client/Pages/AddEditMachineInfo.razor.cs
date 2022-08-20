dcbaa8f4bbe129a09a21ccc4cc87e27a64444e17
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2022-08-20 08:43:08 +0530

    Improved Hardware Components layout

--- Fluid.Client/Pages/AddEditMachineInfo.razor.cs
+++ Fluid.Client/Pages/AddEditMachineInfo.razor.cs
@@ -1,4 +1,5 @@
-﻿using Fluid.Shared.Enums;
+﻿using Fluid.Shared.Entities;
+using Fluid.Shared.Enums;
 using Fluid.Shared.Models;
 using Microsoft.AspNetCore.Components;
 using MudBlazor;
@@ -7,11 +8,55 @@ namespace Fluid.Client.Pages;
 
 public partial class AddEditMachineInfo
 {
-    private HardwareSelectionType motherboardSelectionType = HardwareSelectionType.Existing;
     [Parameter]
     public string Id { get; set; }
-    private SystemConfiguration Model = new SystemConfiguration(); 
+    private SystemConfiguration _model = new SystemConfiguration(); 
     private SystemConfiguration _systemConfiguration = new SystemConfiguration();
+
+    private string _motherboardOemSerialNo;
+    private HardwareSelectionType _motherboardSelectionType = HardwareSelectionType.Existing;
+
+    private async Task LoadMotherboard()
+    {
+        if (string.IsNullOrEmpty(_motherboardOemSerialNo))
+        {
+            snackbar.Add("Please enter a valid Motherboard Serial No");
+            return;
+        }
+        var result = await MotherboardMasterHttpClient.GetByIdAsync(_motherboardOemSerialNo);
+        if (result.Succeeded)
+        {
+            _model.Motherboard = result.Data;
+            snackbar.Add("Motherboard found successfully");
+        }
+        else
+        {
+            foreach (var message in result.Messages)
+            {
+                snackbar.Add(message, Severity.Error);
+            }
+        }
+    }
+
+    private async Task OnMotherboardSelectionType(HardwareSelectionType selectionType)
+    {
+        switch (selectionType)
+        {
+            case HardwareSelectionType.Existing:
+                await LoadMotherboard();
+                break;
+            case HardwareSelectionType.New:
+                _model.Motherboard = new MotherboardInfo();
+                break;
+            case HardwareSelectionType.Empty:
+                _model.Motherboard = null;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(selectionType), selectionType, null);
+        }
+        _motherboardSelectionType = selectionType;
+    }
+    
     protected async override Task OnInitializedAsync()
     {
         if (!string.IsNullOrEmpty(Id))
@@ -19,7 +64,7 @@ public partial class AddEditMachineInfo
             var result = await MasterHttpClient.GetByIdAsync(Id);
             if (result.Succeeded)
             {
-                Model = result.Data;
+                _model = result.Data;
             }
             else
             {
