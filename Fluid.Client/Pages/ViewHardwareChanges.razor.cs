2183baabc282b80ac0e232e7a61804bd7239f61c
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2022-08-21 19:51:32 +0530

    Add ViewHardwareChanges.razor page and improved the table in HardwareLogs.razor

---
+++ Fluid.Client/Pages/ViewHardwareChanges.razor.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+using Fluid.Shared.Models;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace Fluid.Client.Pages;
+
+public partial class ViewHardwareChanges
+{
+    [Parameter]
+    public string Id { get; set; }
+
+    private SystemConfiguration Model { get; set; } = new();
+
+    protected override async Task OnParametersSetAsync()
+    {
+        var result = await MasterHttpClient.GetByIdAsync(Id);
+        if (result.Succeeded)
+        {
+            var feedLog = result.Data;
+            Model = (SystemConfiguration)JsonSerializer.Deserialize(feedLog.JsonRaw, typeof(SystemConfiguration));
+        }
+        else
+        {
+            foreach (var message in result.Messages)
+            {
+                snackbar.Add(message, Severity.Error);
+            }
+            navigationManager.NavigateTo("/Hardware-Logs");
+        }
+        await base.OnParametersSetAsync();
+    }
+}
