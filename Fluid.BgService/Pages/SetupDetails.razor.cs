4e558cd3256d6dfb3a3f003356d8b2074b1594b2
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2022-08-21 13:42:32 +0530

    Fleshed out SetupDetails.razor and added new dialogs for adding hardware components

--- Fluid.BgService/Pages/SetupDetails.razor.cs
+++ Fluid.BgService/Pages/SetupDetails.razor.cs
@@ -1,6 +1,8 @@
-﻿using Fluid.BgService.Services;
+﻿using Fluid.BgService.Dialogs;
+using Fluid.BgService.Services;
 using Fluid.Shared.Entities;
 using Fluid.Shared.Models;
+using MudBlazor;
 
 namespace Fluid.BgService.Pages;
 
@@ -20,6 +22,10 @@ public partial class SetupDetails
 
     private async void Submit()
     {
+        Model.Motherboards.ForEach(x => x.MachineId = Model.MachineDetails.AssetTag);
+        Model.Keyboards.ForEach(x => x.MachineId = Model.MachineDetails.AssetTag);
+        Model.Monitors.ForEach(x => x.MachineId = Model.MachineDetails.AssetTag);
+        Model.Mouses.ForEach(x => x.MachineId = Model.MachineDetails.AssetTag);
         systemConfigurationService.SystemConfiguration = Model;
         var isSucceeded = await systemConfigurationService.LogSystemConfiguration();
         if (isSucceeded)
@@ -30,6 +36,94 @@ public partial class SetupDetails
         {
             Console.WriteLine("SystemConfiguration upload failed");
         }
+    }
+
+    private async Task InvokeMotherboardDialog(MotherboardInfo info)
+    {
+        var parameters = new DialogParameters
+        {
+            { nameof(AddEditMachineMotherboardDialog.Model), info }
+        };
+        var options = new DialogOptions { CloseButton = true, FullWidth = true, DisableBackdropClick = true, Position = DialogPosition.TopCenter };
+        var dialog = dialogService.Show<AddEditMachineMotherboardDialog>("", parameters, options);
+        var result = await dialog.Result;
+        if (result.Cancelled) return;
+        var updatedInfo = result.Data as MotherboardInfo;
+        var oemSerialNo = updatedInfo?.OemSerialNo.Trim();
+        if (Model.Motherboards.Any(x => x.OemSerialNo.Trim() == oemSerialNo))
+            Model.Motherboards.Remove(Model.Motherboards.First(x => x.OemSerialNo.Trim() == oemSerialNo));
+        Model.Motherboards.Add(updatedInfo);
+    }
+
+    private async Task InvokeKeyboardDialog(KeyboardInfo info)
+    {
+        var parameters = new DialogParameters
+        {
+            { nameof(AddEditMachineKeyboardDialog.Model), info }
+        };
+        var options = new DialogOptions { CloseButton = true, FullWidth = true, DisableBackdropClick = true, Position = DialogPosition.TopCenter };
+        var dialog = dialogService.Show<AddEditMachineKeyboardDialog>("", parameters, options);
+        var result = await dialog.Result;
+        if (result.Cancelled) return;
+        var updatedInfo = result.Data as KeyboardInfo;
+        var oemSerialNo = updatedInfo?.OemSerialNo.Trim();
+        if (Model.Keyboards.Any(x => x.OemSerialNo.Trim() == oemSerialNo))
+            Model.Keyboards.Remove(Model.Keyboards.First(x => x.OemSerialNo.Trim() == oemSerialNo));
+        Model.Keyboards.Add(updatedInfo);
+    }
+    
+    private async Task InvokeMonitorDialog(MonitorInfo info)
+    {
+        var parameters = new DialogParameters
+        {
+            { nameof(AddEditMachineMonitorDialog.Model), info }
+        };
+        var options = new DialogOptions { CloseButton = true, FullWidth = true, DisableBackdropClick = true, Position = DialogPosition.TopCenter };
+        var dialog = dialogService.Show<AddEditMachineMonitorDialog>("", parameters, options);
+        var result = await dialog.Result;
+        if (result.Cancelled) return;
+        var updatedInfo = result.Data as MonitorInfo;
+        var oemSerialNo = updatedInfo?.OemSerialNo.Trim();
+        if (Model.Monitors.Any(x => x.OemSerialNo.Trim() == oemSerialNo))
+            Model.Monitors.Remove(Model.Monitors.First(x => x.OemSerialNo.Trim() == oemSerialNo));
+        Model.Monitors.Add(updatedInfo);
+    }
+    
+    private async Task InvokeMouseDialog(MouseInfo info)
+    {
+        var parameters = new DialogParameters
+        {
+            { nameof(AddEditMachineMouseDialog.Model), info }
+        };
+        var options = new DialogOptions { CloseButton = true, FullWidth = true, DisableBackdropClick = true, Position = DialogPosition.TopCenter };
+        var dialog = dialogService.Show<AddEditMachineMouseDialog>("", parameters, options);
+        var result = await dialog.Result;
+        if (result.Cancelled) return;
+        var updatedInfo = result.Data as MouseInfo;
+        var oemSerialNo = updatedInfo?.OemSerialNo.Trim();
+        if (Model.Mouses.Any(x => x.OemSerialNo.Trim() == oemSerialNo))
+            Model.Mouses.Remove(Model.Mouses.First(x => x.OemSerialNo.Trim() == oemSerialNo));
+        Model.Mouses.Add(updatedInfo);
+    }
+    
+    private void DeleteMotherboardInfo(MotherboardInfo motherboardInfo)
+    {
+        Model.Motherboards.Remove(motherboardInfo);
+    }
+    
+    private void DeleteKeyboardInfo(KeyboardInfo keyboardInfo)
+    {
+        Model.Keyboards.Remove(keyboardInfo);
+    }
+    
+    private void DeleteMonitorInfo(MonitorInfo monitorInfo)
+    {
+        Model.Monitors.Remove(monitorInfo);
+    }
+    
+    private void DeleteMouseInfo(MouseInfo mouseInfo)
+    {
+        Model.Mouses.Remove(mouseInfo);
     }
 
     private void AutoFill()
--- Fluid.BgService/Pages/SetupMachine.razor.cs
+++ Fluid.BgService/Pages/SetupMachine.razor.cs
@@ -18,9 +18,9 @@ public partial class SetupMachine
         base.OnInitialized();
     }
 
-    public SetupMachineModel Model { get; set; } = new();
+    private SetupMachineModel Model { get; set; } = new();
 
-    public async void Submit()
+    private async void Submit()
     {
         if (string.IsNullOrWhiteSpace(Model.AssetTag))
         {
@@ -31,14 +31,7 @@ public partial class SetupMachine
             machineIdentifierService.SetAssetTag(Model.AssetTag);
             snackbar.Add("Saved the Asset Tag successfully", MudBlazor.Severity.Success);
             await Task.Delay(1000);
-            if (_redirectToSetupDetails)
-            {
-                navigationManager.NavigateTo("/Setup-Details", true);
-            }
-            else
-            {
-                navigationManager.NavigateTo("/", true);
-            }
+            navigationManager.NavigateTo(_redirectToSetupDetails ? "/Setup-Details" : "/", true);
         }
     }
 }
