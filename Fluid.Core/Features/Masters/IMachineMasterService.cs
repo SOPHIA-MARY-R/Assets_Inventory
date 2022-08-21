6d75a942cc7e99d544b8fc306673fb2f5f5b157b
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2022-08-21 08:51:30 +0530

    Changed the return types in MachineMasterService.cs from Result<string> to Result

--- Fluid.Core/Features/Masters/IMachineMasterService.cs
+++ Fluid.Core/Features/Masters/IMachineMasterService.cs
@@ -5,9 +5,9 @@ namespace Fluid.Core.Features.Masters
 {
     public interface IMachineMasterService
     {
-        Task<Result<string>> AddAsync(MachineInfo model);
-        Task<Result<string>> DeleteAsync(string assetTag);
-        Task<Result<string>> EditAsync(MachineInfo model);
+        Task<IResult> AddAsync(MachineInfo model);
+        Task<IResult> DeleteAsync(string assetTag);
+        Task<IResult> EditAsync(MachineInfo model, string assetTag);
         Task<PaginatedResult<MachineInfo>> GetAllAsync(int pageNumber, int pageSize, string searchString, string orderBy);
         Task<Result<MachineInfo>> GetByIdAsync(string assetTag);
     }
