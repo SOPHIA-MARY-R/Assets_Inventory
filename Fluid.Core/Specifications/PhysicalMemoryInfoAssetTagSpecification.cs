1511548402b497313747799071bc1544d112d28e
Sathiyaraman M <sathiyaraman2003@gmail.com>
2022-08-22 14:33:44 +0530

    Added PhysicalMemoryInfoAssetTagSpecification.cs and HardDiskInfoAssetTagSpecification.cs

---
+++ Fluid.Core/Specifications/PhysicalMemoryInfoAssetTagSpecification.cs
@@ -0,0 +1,19 @@
+﻿using Fluid.Core.Specifications.Base;
+using Fluid.Shared.Entities;
+
+namespace Fluid.Core.Specifications;
+
+public class PhysicalMemoryInfoAssetTagSpecification : BaseSpecification<PhysicalMemoryInfo>
+{
+    public PhysicalMemoryInfoAssetTagSpecification(string assetTag)
+    {
+        if (string.IsNullOrEmpty(assetTag))
+        {
+            FilterCondition = p => true;
+        }
+        else
+        {
+            FilterCondition = p => p.MachineId == assetTag;
+        }
+    }
+}
