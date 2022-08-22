ae13dfe9f53d8aeccf421ed02a5032fe4e913583
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2022-08-22 20:24:45 +0530

    Added new contract interface: IHardwareComponentInfo.cs

---
+++ Fluid.Shared/Contracts/IHardwareComponentInfo.cs
@@ -0,0 +1,22 @@
+﻿using Fluid.Shared.Entities;
+
+namespace Fluid.Shared.Contracts;
+
+public interface IHardwareComponentInfo : IEntity
+{
+    string OemSerialNo { get; set; }
+    
+    string Description { get; set; }
+    
+    HardwareChange HardwareChange { get; set; }
+    
+    UseStatus UseStatus { get; set; }
+    
+    MachineInfo? Machine { get; set; }
+    
+    string? MachineId { get; set; }
+    
+    DateTime? PurchaseDate { get; set; }
+    
+    decimal Price { get; set; }
+}
