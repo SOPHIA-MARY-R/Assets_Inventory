91b08beb6c435de730af98b39b02868b72f343d1
Sathiyaraman M <sathiyaraman2003@gmail.com>
2022-08-23 23:12:55 +0530

    Added HardwareChangeLogController.cs

---
+++ Fluid.Server/Controllers/HardwareChangeLogController.cs
@@ -0,0 +1,23 @@
+﻿using Fluid.Core.Features;
+using Fluid.Shared.Models.FilterModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fluid.Server.Controllers;
+
+[ApiController]
+[Route("api/hardware-changelogs")]
+public class HardwareChangeLogController : ControllerBase
+{
+    private readonly IHardwareChangeLogService _hardwareChangeLogService;
+
+    public HardwareChangeLogController(IHardwareChangeLogService hardwareChangeLogService)
+    {
+        _hardwareChangeLogService = hardwareChangeLogService;
+    }
+
+    [HttpPost("get-all")]
+    public async Task<IActionResult> GetAllAsync(HardwareChangeLogFilter hardwareChangeLogFilter)
+    {
+        return Ok(await _hardwareChangeLogService.GetHardwareChangeLogsAsync(hardwareChangeLogFilter));
+    }
+}
