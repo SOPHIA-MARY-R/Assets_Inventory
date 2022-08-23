27ace8f35919d47c96a3e01f5b275c5e7057ccb1
Sathiyaraman M <sathiyaraman2003@gmail.com>
2022-08-23 23:12:41 +0530

    Added HardwareChangeLogService.cs

---
+++ Fluid.Core/Features/IHardwareChangeLogService.cs
@@ -0,0 +1,9 @@
+﻿using Fluid.Shared.Entities;
+using Fluid.Shared.Models.FilterModels;
+
+namespace Fluid.Core.Features;
+
+public interface IHardwareChangeLogService
+{
+    Task<PaginatedResult<HardwareChangeLog>> GetHardwareChangeLogsAsync(HardwareChangeLogFilter filter);
+}
