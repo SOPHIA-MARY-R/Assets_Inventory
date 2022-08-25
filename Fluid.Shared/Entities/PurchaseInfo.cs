c0d4ac8e306618e0cfff8d87ba652d3305542d55
SOPHIA MARY R <soph20410.cs@rmkec.ac.in>
2022-08-25 16:32:41 +0530

    Added new entities: PurchaseInfo and PurchaseItem

---
+++ Fluid.Shared/Entities/PurchaseInfo.cs
@@ -0,0 +1,21 @@
+﻿namespace Fluid.Shared.Entities;
+
+public class PurchaseInfo
+{
+    [Key]
+    public string InvoiceNo { get; set; }
+
+    public DateTime PurchaseDate { get; set; }
+
+    public string VendorName { get; set; }
+
+    public string Salesman { get; set; }
+
+    public string BoughtThrough { get; set; }
+
+    public List<PurchaseItem> PurchaseItems { get; set; } = new List<PurchaseItem>();
+
+    public decimal GrandTotal => PurchaseItems.Select(x => x.NetRate * x.Quantity).Sum();
+
+    public string TechnicianUserId { get; set; }
+}
---
+++ Fluid.Shared/Entities/PurchaseItem.cs
@@ -0,0 +1,25 @@
+﻿namespace Fluid.Shared.Entities;
+
+public class PurchaseItem
+{
+    [Key]
+    public string HSN { get; set; }
+
+    public string SerialNumber { get; set; }
+
+    public string Manufacturer { get; set; }
+
+    public string ItemName { get; set; }
+
+    public int Quantity { get; set; }
+
+    public decimal NetRate { get; set; }
+
+    public decimal Price { get; set; }
+
+    public decimal Amount => Price * Quantity;
+
+    public PurchaseInfo PurchaseInfo { get; set; }
+
+    public string PurchaseInfoId { get; set; }
+}
