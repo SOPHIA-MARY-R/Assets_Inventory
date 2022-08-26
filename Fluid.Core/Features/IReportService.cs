fb5bb4bf4ffe0d983ca3e1da5792e3600b95d435
Sathiyaraman M <sathiyaraman2003@gmail.com>
2022-08-26 13:29:22 +0530

    Added ReportService

---
+++ Fluid.Core/Features/IReportService.cs
@@ -0,0 +1,13 @@
+﻿using Fluid.Shared.Models.FilterModels;
+
+namespace Fluid.Core.Features;
+
+public interface IReportService
+{
+    Task<IResult<string>> ExportToExcelAsync<T>(IEnumerable<T> data,
+        Dictionary<string, Func<T, object>> mappings,
+        string sheetName = "sheet1");
+
+    Task<IResult<string>> ExportFeedLogsAsync(FeedLogFilter filter);
+    Task<IResult<string>> ExportHardwareChangeLogsAsync(HardwareChangeLogFilter filter);
+}
